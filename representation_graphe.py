b7f7363ea86e9ced3a3af25f2869ca82011f6d38
Julien <jdubocag@0680034t.ac-strasbourg.fr>
2018-12-07 09:51:23 +0100

    essai2

--- representation_graphe.py
+++ representation_graphe.py
@@ -1,13 +1,13 @@
 n = 6 # Nombre de sommets du graphe
 
-matAdj = [[0,1,1,0,0,0],
-          [0,0,0,0,1,0],
-          [1,0,0,1,0,0],
-          [0,1,0,0,0,0],
-          [0,0,0,0,0,0],
-          [0,1,0,1,1,0]]
+matAdj = [[0,1,0,1],
+          [1,0,0,0],
+          [1,1,0,0],
+          [0,0,0,1]]
 
 def matAdjTabSuccess(n):
     # Produit les tableaux aps et fs des succes
 
     global aps, fs
+    aps = [0,3,5,8]
+    fs = [2,4,0,1,0,1,2,0,4,0]
