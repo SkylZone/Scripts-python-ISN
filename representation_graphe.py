c2c5770c67a6dd8385335c216a913ae1a87fccbd
Julien <jdubocag@0680034t.ac-strasbourg.fr>
2018-12-07 10:01:32 +0100

    correction

--- representation_graphe.py
+++ representation_graphe.py
@@ -9,5 +9,13 @@ def matAdjTabSuccess(n):
     # Produit les tableaux aps et fs des succes
 
     global aps, fs
-    aps = [0,3,5,8]
-    fs = [2,4,0,1,0,1,2,0,4,0]
+
+    k = 0
+    for i in range(n):
+        aps[i] = k
+        for j in range(n):
+            if matAdj[i][j] != 0:
+                fs.append(j+1)
+                k += 1
+        fs.append(0)
+        k += 1
